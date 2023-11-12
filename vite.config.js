64b11bff6064140a2621938ee3aa5737cef7f868
sakshi-1604 <sddahake16@gmail.com>
2023-11-12 12:31:26 +0530

    Add

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [react()],
   css:{
     modules:{
+
       localsConvention: "camelCase",
     }
   }
